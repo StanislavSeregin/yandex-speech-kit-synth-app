105c10e536dd69a8c2eacc6db9d8c9808558ad3b
StanislavSeregin <stas_seregin@mail.ru>
2022-12-20 14:44:11 +0300

    Init solution

---
+++ src/backend/YandexSpeechKitSynthClient.Api/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace YandexSpeechKitSynthClient.Api;
+
+internal class Program
+{
+    public static Task Main(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder
+                .UseStartup<Startup>())
+            .Build()
+            .RunAsync();
+    }
+}
---
+++ src/backend/YandexSpeechKitSynthClient.Api/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using System;
+
+namespace YandexSpeechKitSynthClient.Api;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddCors()
+            .AddControllers();
+
+        services
+            .AddLogging();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app
+            .UseRouting()
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            })
+            .UseStaticFiles()
+            .UseSpa(spaBuilder =>
+            {
+                var cacheControlHeaderValue = new CacheControlHeaderValue
+                {
+                    NoCache = true,
+                    NoStore = true,
+                    MustRevalidate = true,
+                    MaxAge = TimeSpan.Zero
+                };
+
+                var staticFileOptions = new StaticFileOptions
+                {
+                    OnPrepareResponse = ctx =>
+                    {
+                        var headers = ctx.Context.Response.GetTypedHeaders();
+                        headers.CacheControl = cacheControlHeaderValue;
+                    }
+                };
+
+                spaBuilder.Options.DefaultPageStaticFileOptions = staticFileOptions;
+            });
+    }
+}
